7d62cfdcc7ec9c712250d8f4867c20bce2605db2
David Lin <dl3061@rit.edu>
2020-03-01 15:09:33 -0500

    Adding Input Manager.

---
+++ Assets/Prefabs/InputManager/InputManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Singleton
+    {
+        get; private set;
+    }
+
+
+    // Variables
+    private Vector2 move;
+    private bool moveLeft;
+    private bool moveLeftDown;
+    private bool moveRight;
+    private bool moveRightDown;
+    private bool jump;
+    private bool jumpDown;
+
+
+    /// <summary>
+    /// Awake is called before start. 
+    /// Initialize references here.
+    /// </summary>
+    private void Awake()
+    {
+        InputManager.Singleton = this;
+    }
+
+
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// Initialize values here.
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    #region Accessors
+
+    public Vector2 GetMove()
+    {
+        return move;
+    }
+
+    public bool GetMoveLeft()
+    {
+        return moveLeft;
+    }
+
+    public bool GetMoveLeftDown()
+    {
+        return moveLeftDown;
+    }
+
+    public bool GetMoveRight()
+    {
+        return moveRight;
+    }
+
+    public bool GetMoveRightDown()
+    {
+        return moveRightDown;
+    }
+
+    #endregion
+}
