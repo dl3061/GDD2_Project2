57ccc4982c3e2cd72761938dfaa6971bb7189cf9
Jamal Woods (RIT Student) <jcw2298@ad.rit.edu>
2020-03-01 16:30:41 -0500

    Added TileDirector script, Formation Class, and Formation Scriptable Object

---
+++ Assets/Classes/FormationClass.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Formation")]
+public class Formation : ScriptableObject
+{
+    public List<Vector3[]> formationDetails;
+}
---
+++ Assets/Scripts/TileDirector.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileDirector : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    [SerializeField]
+    List<GameObject> tiles;
+    Formation currentFormation;
+
+    private void Start()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            GameObject tile = Instantiate(tilePrefab);
+            tile.SetActive(false);
+            tiles.Add(tile);
+        }
+    }
+
+    GameObject getReadyTile()
+    {
+        GameObject returnObj = null;
+        foreach(GameObject t in tiles)
+        {
+            if (!t.activeSelf)
+            {
+                returnObj = t;
+            }
+        }
+        return returnObj;
+        
+    }
+
+    private void Update()
+    {
+        if (currentFormation)
+        {
+            for(int i = 0; i < currentFormation.formationDetails.Count; i++)
+            {
+                //Loop through formation details and re-create tiles based on instructions 
+            }
+        }
+    }
+
+}
