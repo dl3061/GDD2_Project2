a345a1ef24d7d03b3b60484f684ed2ef7652237c
David Lin <dl3061@rit.edu>
2020-03-01 15:09:21 -0500

    Added polarity controller.

---
+++ Assets/CommonScripts/Enum/Polarity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enumeration for Polarities (neutral, light, dark, etc)
+/// </summary>
+public enum Polarity
+{
+    Neutral = 0,
+    Light = 1,
+    Dark = 2
+}
---
+++ Assets/CommonScripts/PolarityToggle.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This monobehavior is responsible from toggling active/inactive gameobjects depending on polarity.
+///     
+/// </summary>
+public class PolarityToggle : MonoBehaviour
+{
+    [Tooltip("The polarity to be initialized to. If Neutral, this script does nothing.")]
+    public Polarity defaultPolarity = Polarity.Neutral;
+
+    [Tooltip("The gameobject that appears when in lightside.")]
+    public GameObject lightObject;
+
+    [Tooltip("The gameobject that appears when in darkside.")]
+    public GameObject darkObject;
+
+
+    // The current polarity
+    private Polarity currentPolarity;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        currentPolarity = defaultPolarity;
+    }
+
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        // Reinforce the current active polarity, unless neutral.
+        if (currentPolarity != Polarity.Neutral)
+        {
+            if (lightObject != null)
+                lightObject.SetActive(currentPolarity == Polarity.Light);
+
+            if (darkObject != null)
+                darkObject.SetActive(currentPolarity == Polarity.Dark);
+        }
+    }
+
+
+    /// <summary>
+    /// Reset Event Handler. Call when resetting (ie restarting) the game.
+    /// </summary>
+    public void ResetEvent()
+    {
+        currentPolarity = defaultPolarity;
+    }
+
+
+    /// <summary>
+    /// Toggles the polarity to the opposite polarity.
+    /// </summary>
+    public void TogglePolarity()
+    {
+        if (currentPolarity == Polarity.Light)
+        {
+            currentPolarity = Polarity.Dark;
+        }
+        else if (currentPolarity == Polarity.Dark)
+        {
+            currentPolarity = Polarity.Light;
+        }
+        else
+        {
+            // Neutral case. Do nothing.
+        }
+    }
+
+
+    /// <summary>
+    /// Forcibly sets the polarity to light side.
+    /// </summary>
+    public void SetPolarityToLight()
+    {
+        currentPolarity = Polarity.Light;
+    }
+
+
+    /// <summary>
+    /// Forcibly sets the polarity to dark side.
+    /// </summary>
+    public void SetPolarityToDark()
+    {
+        currentPolarity = Polarity.Dark;
+    }
+}
