9eca7d9a8eac188f092b1a73f14edc26bbaea084
David Lin <dl3061@rit.edu>
2020-03-01 14:17:54 -0500

    Added Game Events and Game Event Listeners.

---
+++ Assets/CommonScripts/GameEvents/GameEvent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Scriptable object to trigger a game event
+/// </summary>
+[CreateAssetMenu(fileName = "Game Event", menuName = "Game Event", order = 52)]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> listeners = new List<GameEventListener>();
+
+    /// <summary>
+    /// Raises the event of all listeners for the game event
+    /// </summary>
+    public void Raise()
+    {
+        for (int c = 0; c < listeners.Count; c++)
+        {
+            listeners[c].OnEventRaised();
+        }
+    }
+
+    /// <summary>
+    /// Registers a listener for the event
+    /// </summary>
+    /// <param name="listener">The listener to register</param>
+    public void RegisterListener(GameEventListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    /// <summary>
+    /// Unregisters a listener for the event
+    /// </summary>
+    /// <param name="listener">The listener to unregister</param>
+    public void UnregisterListener(GameEventListener listener)
+    {
+        listeners.Remove(listener);
+    }
+}
---
+++ Assets/CommonScripts/GameEvents/GameEventListener.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Script to respond when a GameEvent is triggered
+/// </summary>
+public class GameEventListener : MonoBehaviour
+{
+    [SerializeField]
+    private GameEvent gameEvent = null;
+    [SerializeField]
+    private UnityEvent response = null;
+
+    /// <summary>
+    /// Registers the listener when the object is enabled
+    /// </summary>
+    private void OnEnable()
+    {
+        if (gameEvent != null)
+        {
+            gameEvent.RegisterListener(this);
+        }
+    }
+
+    /// <summary>
+    /// Unregisters the listener when the object is disabled
+    /// </summary>
+    private void OnDisable()
+    {
+        if (gameEvent != null)
+        {
+            gameEvent.UnregisterListener(this);
+        }
+    }
+
+    /// <summary>
+    /// Raises the appropriate event for the listener when called
+    /// </summary>
+    public void OnEventRaised()
+    {
+        if (response != null)
+        {
+            response.Invoke();
+        }
+    }
+}
