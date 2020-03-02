3af5e9094f11c1fcbd0bfac1562e12638f27277d
David Lin (RIT Student) <dl3061@ad.rit.edu>
2020-03-02 13:43:10 -0500

    Added a GetLength() function to the FormationClass.

--- Assets/CommonScripts/Classes/FormationClass.cs
+++ Assets/CommonScripts/Classes/FormationClass.cs
@@ -16,4 +16,35 @@ public class Formation : ScriptableObject
     }
 
     public List<formationDetail> formationDetails;
+
+
+    /// <summary>
+    /// Calculates the length of the formation along the z-axis.
+    /// </summary>
+    /// <returns></returns>
+    public float GetLength()
+    {
+        float minZ = float.PositiveInfinity;
+        float maxZ = float.NegativeInfinity;
+
+        foreach (formationDetail piece in formationDetails)
+        {
+            // Get the starting and end z corrdinates of this piece
+            float currMinZ = piece.formationPositionDetails.z - (piece.formationScaleDetails.z / 2f);
+            float currMaxZ = piece.formationPositionDetails.z + (piece.formationScaleDetails.z / 2f);
+
+            // Save it to the edges of the overall formation
+            if (currMinZ < minZ || float.IsPositiveInfinity(minZ))
+                minZ = currMinZ;
+
+            if (currMaxZ > maxZ || float.IsNegativeInfinity(maxZ))
+                maxZ = currMaxZ;
+        }
+
+        float length = maxZ - minZ;
+        if (length < 0f)
+            length = (-1f) * length;
+
+        return length;
+    }
 }
