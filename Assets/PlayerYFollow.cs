7a844860c38cfe1b36f5f92fc0c7bc22c429a9ea
Jamal <jamalcwoods@gmail.com>
2020-03-05 16:28:10 -0500

    Locked Elevated Camera

---
+++ Assets/PlayerYFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerYFollow : MonoBehaviour
+{
+    // Start is called before the first frame 
+    [SerializeField]
+    GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, player.transform.position.y + 3, transform.position.z);
+    }
+}
